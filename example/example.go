406a0a849fa28f606221c9046038038b1a2a512d
Guo Yu <i@guoyu.me>
2014-12-06 21:04:04 +0800

    Add example

--- example/example.go
+++ example/example.go
@@ -1,3 +1,22 @@
-package example
+package main
 
 import "os"
+
+var testsJs = []string{
+	"./static/js/a.js",
+	"./static/js/b.js",
+}
+
+var testCss = []string{
+	"./static/css/a.css",
+	"./static/css/b.css",
+}
+
+func main() {
+	// Combo test static: js
+	comboJs, err := Combo(testsJs)
+	// Combo test static: css
+	comboCss, err2 := Combo(testCss)
+	// Combo test static: js to dist file
+	err3 := ComboTo("./static/js/dist.js", testsJs)
+}
